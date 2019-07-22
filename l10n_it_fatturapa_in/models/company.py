5c1c93574a9486ef1b153b83f07b18b34bd706d9
Roberto Fichera <robyf@tekno-soft.it>
2019-07-22 20:23:11 +0200

    [10.0][IMP][l10n_it_fatturapa_in] Add active and passive rounding support when importing e-bill

--- l10n_it_fatturapa_in/models/company.py
+++ l10n_it_fatturapa_in/models/company.py
@@ -14,6 +14,21 @@ class ResCompany(models.Model):
         'product.product', 'Discount Supplement Product',
         help="Product used to model ScontoMaggiorazione XML element on bills."
         )
+    arrotondamenti_attivi_account_id = fields.Many2one(
+        'account.account', 'Active Rounding Account',
+        domain=[('deprecated', '=', False)],
+        help="Account used for active rounding amount on bills."
+        )
+    arrotondamenti_passivi_account_id = fields.Many2one(
+        'account.account', 'Passive Rounding Account',
+        domain=[('deprecated', '=', False)],
+        help="Account used for passive rounding amount on bills."
+        )
+    arrotondamenti_tax_id = fields.Many2one(
+        'account.tax', 'Rounding Tax',
+        domain=[('type_tax_use', '=', 'purchase'), ('amount', '=', 0.0)],
+        help="Tax used for rounding amount on bills."
+        )
 
 
 class AccountConfigSettings(models.TransientModel):
@@ -29,6 +44,27 @@ class AccountConfigSettings(models.TransientModel):
         string="Discount Supplement Product",
         help="Product used to model ScontoMaggiorazione XML element on bills."
         )
+    arrotondamenti_attivi_account_id = fields.Many2one(
+        related='company_id.arrotondamenti_attivi_account_id',
+        string='Active Rounding Account',
+        domain=[('deprecated', '=', False)],
+        help="Account used for active rounding amount on bills.",
+        readonly=False,
+        )
+    arrotondamenti_passivi_account_id = fields.Many2one(
+        related='company_id.arrotondamenti_passivi_account_id',
+        string='Passive Rounding Account',
+        domain=[('deprecated', '=', False)],
+        help="Account used for passive rounding amount on bills.",
+        readonly=False,
+        )
+    arrotondamenti_tax_id = fields.Many2one(
+        related='company_id.arrotondamenti_tax_id',
+        domain=[('type_tax_use', '=', 'purchase'), ('amount', '=', 0.0)],
+        string='Rounding Tax',
+        help="Tax used for rounding amount on bills.",
+        readonly=False,
+        )
 
     @api.onchange('company_id')
     def onchange_company_id(self):
@@ -43,7 +79,22 @@ class AccountConfigSettings(models.TransientModel):
                 company.sconto_maggiorazione_product_id and
                 company.sconto_maggiorazione_product_id.id or False
                 )
+            self.arrotondamenti_attivi_account_id = (
+                    company.arrotondamenti_attivi_account_id and
+                    company.arrotondamenti_attivi_account_id.id or False
+            )
+            self.arrotondamenti_passivi_account_id = (
+                    company.arrotondamenti_passivi_account_id and
+                    company.arrotondamenti_passivi_account_id.id or False
+            )
+            self.arrotondamenti_tax_id = (
+                    company.arrotondamenti_tax_id and
+                    company.arrotondamenti_tax_id.id or False
+            )
         else:
             self.cassa_previdenziale_product_id = False
             self.sconto_maggiorazione_product_id = False
+            self.arrotondamenti_attivi_account_id = False
+            self.arrotondamenti_passivi_account_id = False
+            self.arrotondamenti_tax_id = False
         return res
